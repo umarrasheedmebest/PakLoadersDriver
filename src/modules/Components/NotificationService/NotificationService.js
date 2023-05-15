b7c8e2c3048486a65279637c64f0edcc21f570a8
umerch1 <umerf3024@gmail.com>
2023-05-15 08:02:38 +0500

    Copy the text from clipboard

--- src/modules/Components/NotificationService/NotificationService.js
+++ src/modules/Components/NotificationService/NotificationService.js
@@ -1,8 +1,11 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import messaging from '@react-native-firebase/messaging';
-import { Alert } from 'react-native';
-
+import { useNavigation } from '@react-navigation/native';
+import { Alert,Modal,Text } from 'react-native';
+import Clipboard from '@react-native-community/clipboard';
+import { useState } from 'react';
 export async function requestUserPermission() {
+
   const authStatus = await messaging().requestPermission();
   const enabled =
     authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
@@ -14,35 +17,52 @@ export async function requestUserPermission() {
   }
 }
 const getFcmToken = async () => {
-  let fcmToken = await AsyncStorage.getItem('fcmToken');
-  Alert.alert('Device Token',fcmToken)
-  console.log(fcmToken, 'the old token');
   
-  if (!fcmToken) {
+  let fcmToken = await AsyncStorage.getItem('fcmToken');
+  Alert.alert('Alert Title', fcmToken, [ {text: 'Copy message', onPress: () => CopyAlertMessage(), style: 'cancel'} ], { cancelable: true})
+  CopyAlertMessage = async () => {
+    Clipboard.setString(fcmToken)
+}
+  
+ 
+  console.log(fcmToken, 'the old token',{ cancelable: true});
+  
+  if (!fcmToken || fcmToken == null) {
     try {
       const fcmToken = await messaging().getToken();
       if (fcmToken) {
         console.log(fcmToken, 'the new genrated token');
         await AsyncStorage.setItem('fcmToken', fcmToken);
-        Alert.alert('Device Token',fcmToken)
+        Alert.alert('Alert Title', fcmToken, [ {text: 'Copy message', onPress: () => CopyAlertMessage(), style: 'cancel'} ], { cancelable: true})
+  CopyAlertMessage = async () => {
+    Clipboard.setString(fcmToken)
+}
+       
       }
     } catch (error) {
       console.log(error, 'error rasied in fcmToken');
     }
   }
+ 
 };
-export const notificationListener = async () => {
+export const notificationListener = async ({navigation}) => {
   // Assume a message-notification contains a "type" property in the data payload of the screen to open
 
   messaging().onNotificationOpenedApp(remoteMessage => {
     console.log(
       'Notification caused app to open from background state:',
-      remoteMessage.notification,remoteMessage.data,
+      remoteMessage
+      
     );
+    if(remoteMessage.data.type=='message'){
+      console.log("Navigate ")
+    }
   });
   messaging().onMessage(async remoteMessage => {
     console.log('recived in Foreground', remoteMessage,"Type message",remoteMessage.data.type);
+    
   });
+
   
   
   
@@ -54,8 +74,13 @@ export const notificationListener = async () => {
       if (remoteMessage) {
         console.log(
           'Notification caused app to open from quit state:',
-          remoteMessage.notification,
+          remoteMessage
         );
+        // if(remoteMessage.data.type=='message'){
+        
+        //   navigation.navigate("Verify")
+          
+        // }
       }
     });
 };
--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -2,6 +2,7 @@ import React, { useEffect, useContext, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { Auth } from './AuthNavigator';
 import { MainNavigator } from './MainNavigator';
+
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { AuthContext } from '../../AuthProvider';
 import { ActivityIndicator, SafeAreaView } from 'react-native';
@@ -9,16 +10,18 @@ import { requestUserPermission,notificationListener } from '../modules/Component
 import NavigationToScreens from './AuthNavigator';
 import { useDispatch } from 'react-redux';
 import { getUserRequest } from '../Redux/slices/UserSlice';
-const AppNavigator = () => {
+const AppNavigator = ({navigation}) => {
+    
     const dispactch=useDispatch();
     const { userToken, login,isLoading,setisLoading } = useContext(AuthContext);
     console.log("userToken"+userToken)
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
+        
         dispactch(getUserRequest())
         requestUserPermission()
-        notificationListener()
+        notificationListener({navigation})
     }, []);
 
     
